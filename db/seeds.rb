1596b5f79b8fdec239a5fa0480e3d04ea58efc81
张保良 <jfreebird@gmail.com>
2012-06-07 18:06:25 +0800

    去掉wiki + 修发初始化数据

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,7 @@ SiteConfig.save_default("index_html",<<-eos
 <div class="box" style="text-align:center;">
   <p><img alt="Big_logo" src="/assets/big_logo.png"></p>
   <p></p>
-  <p>Ruby China Group， 致力于构建完善的 Ruby 中文社区。</p>
-  <p>功能正在完善中，欢迎 <a href="http://github.com/huacnlee/ruby-china">贡献代码</a> 。</p>
-  <p>诚邀有激情的活跃 Ruby 爱好者参与维护社区，联系 <b style="color:#c00;">lgn21st@gmail.com</b></p>
+  <p>农友圈， 致力于构建完善的农业社区。</p>
 </div>
 eos
 )
@@ -26,7 +24,7 @@ eos
 # Footer HTML
 SiteConfig.save_default("footer_html",<<-eos
 <p class="copyright">
- &copy; Ruby China Group.
+ &copy; jfreebird@gmail.com.
 </p>
 eos
 )
